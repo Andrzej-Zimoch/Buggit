0d991d2427c1a752d0822475c90fdd990f6c9857
andrzejzimoch4 <andrzejzimoch4@gmail.com>
2021-04-07 21:58:05 +0200

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = '#uj(dj46%lqx5b%pz=ki(l!_g43$1!5p8o)n&8&$0&(3w%bh9i'
 # SECRET_KEY = os.environ['SECRET_KEY']
-SECRET_KEY = '#uj(dj46%lqx5b%pz=ki(l!_g43$1!5p8o)n&8&$0&(3w%bh9i'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -41,11 +41,13 @@ ALLOWED_HOSTS = ['buggito.herokuapp.com']
 
 EMAIL_USE_TLS = True
 
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = os.environ['EMAIL_HOST']
 
-EMAIL_HOST_USER = 'buggitoapp@gmail.com'
-EMAIL_HOST_PASSWORD = 'Agnieszka21!'
-EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+
+EMAIL_PORT = os.environ['EMAIL_PORT']
 # Application definition
 
 USE_DJANGO_JQUERY = True
