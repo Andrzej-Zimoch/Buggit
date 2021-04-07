cdc400c8d26689c4d4d85fbe9255fb92f11f7a28
andrzejzimoch4 <andrzejzimoch4@gmail.com>
2021-04-07 22:54:43 +0200

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://buggito.herokuapp.com/']
+ALLOWED_HOSTS = ['buggito.herokuapp.com']
 
 EMAIL_USE_TLS = True
 
