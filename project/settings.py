451f7fa3fcc2f5b94a52a9ac9b6ceaca8390c1fa
Andrzej Zimoch <53918289+Andrzej-Zimoch@users.noreply.github.com>
2021-04-08 22:14:14 +0200

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -35,7 +35,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ['DEBUG']
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
