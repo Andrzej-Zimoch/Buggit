6c8b06789bbf3a2e00053ff51dbc1197f6c59dc0
Andrzej Zimoch <53918289+Andrzej-Zimoch@users.noreply.github.com>
2021-04-08 22:10:16 +0200

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -36,7 +36,21 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
 ALLOWED_HOSTS = ['buggito.herokuapp.com']
 
 EMAIL_USE_TLS = True
