cbdd5851cebd53b3f664ea653347ee692eb3c2b5
Andrzej Zimoch <53918289+Andrzej-Zimoch@users.noreply.github.com>
2021-04-11 20:28:14 +0200

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -177,6 +177,19 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 AUTH_PROFILE_MODULE = 'bug.Profile'
 
+
+from django.views.decorators.csrf import requires_csrf_token
+from django.http import (
+    HttpResponseBadRequest, HttpResponseForbidden, HttpResponseNotFound,
+    HttpResponseServerError,)
+@requires_csrf_token
+def my_customized_server_error (request, template_name = '500.html'):
+    import sys
+    from django.views import debug
+    error_html = debug.technical_500_response (request, * sys.exc_info ()). content
+    return HttpResponseServerError (error_html)
+
+
 django_heroku.settings(locals())
 
 
