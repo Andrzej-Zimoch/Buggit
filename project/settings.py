5d6542be0defd982f2177bf5f589dcd6c1ad2efe
Andrzej Zimoch <53918289+Andrzej-Zimoch@users.noreply.github.com>
2021-04-11 20:39:21 +0200

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 #DEBUG = bool(os.environ.get("DEBUG_VAL", False))
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://buggito.herokuapp.com']
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 EMAIL_USE_TLS = True
 
@@ -177,6 +177,44 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 AUTH_PROFILE_MODULE = 'bug.Profile'
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': ('%(asctime)s [%(process)d] [%(levelname)s] '
+                       'pathname=%(pathname)s lineno=%(lineno)s '
+                       'funcname=%(funcName)s %(message)s'),
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        }
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+        'django.request': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+    }
+}
 
 
 
